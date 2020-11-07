889da6213c8994322a144eb70cdfcd9acf75f8c9
ExplodingN <66927049+ExplodingN@users.noreply.github.com>
2020-11-07 15:12:24 -0600

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,1 +1,17 @@
+function Snake(){
+	this.x = 0;
+	this.y = 0;
+	this.xSpeed = scale *1;
+	this.ySpeed = 0;
 
+
+	this.draw = function(){
+		ctx.fillStyle - "#FFFFFF";
+		ctx.fillRect(this.x,this.y,scale,scale);
+	}
+
+	this.update = function(){
+		this.x += this.xSpeed;
+		this.y += this.ySpeed;
+	}
+}
