5aceade43975b4557533db54df7ee74890143c93
ExplodingN <66927049+ExplodingN@users.noreply.github.com>
2020-11-07 03:40:46 -0600

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,1 +1,8 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
 
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
